e57257a755a398467f0ba3652343d6eeeb854b77
phlthy88 <joshualfrazier@gmail.com>
2025-12-11 14:10:49 -0500

    fix: final vitest config correction

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     pool: 'forks',
-    minWorkers: 1,
     maxWorkers: 2,
     fileParallelism: false,
     coverage: {
